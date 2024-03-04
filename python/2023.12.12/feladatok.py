7a416b6a34c4e81679cfd0f3e61202ece2209ba4
bayerbalint <147805002+bayerbalint@users.noreply.github.com>
2024-03-04 17:15:28 +0100

    Add files via upload

--- python/2023.12.12/feladatok.py
+++ python/2023.12.12/feladatok.py
@@ -1,19 +1,6 @@
 # 1. feladat
 def reszekre_osztas(mondat):
-    E = ""
-    K = ""
-    V = ""
-    for index, i in enumerate(mondat):
-        if index % (len(mondat) // 3) == 0 and E == "" and K == "":
-            E = mondat[:index]
-        if index % (len(mondat) // 3) == 0 and E != "" and K == "":
-            K = mondat[len(E):index]
-        if index % (len(mondat) // 3) == 0 and E != "" and K != "":
-            if index != len(mondat):
-                V = mondat[len(K) + len(E):index+len(mondat)%3]
-            else:
-                V = mondat[len(K) + len(E):index]
-    return K + V + E
+    return mondat[len(mondat)//3:len(mondat)*2//3] + mondat[len(mondat)*2//3:] + mondat[:len(mondat)//3]
     
 
 print(reszekre_osztas(input("Kérek egy mondatot! ")))
